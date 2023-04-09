889ecd80736a860685b2626daf45a323d45ae390
John Michael <johnfrontend@gmail.com>
2023-04-09 00:04:48 -0300

    Classes Cliente, Carro,Vendedor e Venda

---
+++ Loja de carros/Models/Carro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_de_carros.Models
+{
+    [Table("Carro do Ano")]
+    public class Carro
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Display(Name = "Modelo")]
+        public string Modelo { get; set; }
+
+        [Display(Name = "Marca")]
+        public string Marca { get; set; }
+
+        [Display(Name = "Ano")]
+        public string Ano { get; set; }
+
+        [Display(Name = "Combustivel")]
+        public string Tipo { get; set; }
+
+        [Display(Name = "Valor")]
+        public float Preco { get; set; }
+    }
+}
---
+++ Loja de carros/Models/Vendedor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Loja_de_carros.Models
+{
+    [Table("Tabela de Vendedor")]
+    public class Vendedor
+    {           
+
+            [Display(Name = "Identificador")]
+            public int Id { get; set; }
+
+
+            [Display(Name = "Nome")]
+            public String Nome { get; set; }
+
+            [Display(Name = "Telefone")]
+            public String Telefone { get; set; }
+            
+            [Display(Name = "Endereço")]
+            public String Endereco { get; set; }
+
+            [Display(Name = "E-mail")]
+            public String Email { get; set; }
+        }
+}

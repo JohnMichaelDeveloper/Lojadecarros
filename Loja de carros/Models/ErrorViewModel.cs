80cf581f4ebd9505608616a03d309f88185502c9
John Michael <johnfrontend@gmail.com>
2023-04-08 23:29:36 -0300

    Projeto Base

---
+++ Loja de carros/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Loja_de_carros.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
